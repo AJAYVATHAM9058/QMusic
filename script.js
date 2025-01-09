3329d8b974ff33c79d59727cfe8107167ca83033
AJAY VATHAM <ajayvatham31@gmail.com>
2025-01-09 19:00:22 +0530

    Modified

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let play_pause = document.getElementById("play_pause");
+let audio_element = new Audio("./Songs/lightOfSevenSong2.mp3");
 
-const res = document.querySelectorAll("#play")
-console.log(res)
+play_pause.addEventListener("click",()=>{
+
+    if(audio_element.paused || audio_element.currentTime==0){
+        audio_element.play()
+
+        play_pause.classList.remove("fa-play")
+        play_pause.classList.add("fa-pause")
+    }
+    else{
+        audio_element.pause()
+        play_pause.classList.remove("fa-pause");
+        play_pause.classList.add("fa-play");
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
