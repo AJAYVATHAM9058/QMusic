7154fb72fc8f6919298e64ad937b093f979a1a96
AJAY VATHAM <ajayvatham31@gmail.com>
2025-01-21 10:54:00 +0530

    Modified

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
 let play_pause = document.getElementById("play_pause");
 let audio_element = new Audio("./Songs/lightOfSevenSong2.mp3");
 let song_progress_bar = document.getElementById("song_progress_bar");
+let play_buttons = document.querySelectorAll("#play_solid i")
+let currentPlayingIndex = -1
+let album_name = document.getElementById("album_name")
+
+let time = document.querySelector("#time")
+
+let songs = [
+    "./Songs/song1.mp3","./Songs/song2.mp3","./Songs/song3.mp3","./Songs/song4.mp3","./Songs/song5.mp3","./Songs/song6.mp3","./Songs/song7.mp3",
+    "./Songs/song8.mp3","./Songs/song9.mp3","./Songs/song10.mp3","./Songs/song11.mp3","./Songs/song12.mp3",
+
+]
+
+let songNames = [
+    "Chuttamlle", "Oh Priya Priya", "Konte Chooputho", "Tum Hi Ho", "Ban Ja Rani", "Ban Ja Rani Remix", "Animal(Telugu)", "Yeh Jawaani hai Deewani", "Peelings",
+    "Panjaa", "Godari Gattu Meeda", "Radhika Radhika"
+]
+
+play_buttons.forEach((button, index) => {
+
+    button.addEventListener("click", function () {
+
+        if (currentPlayingIndex !== index) {
+
+            if (currentPlayingIndex !== -1) {
+                play_buttons[currentPlayingIndex].classList.replace("fa-pause", "fa-play")
+                
+            }
+          
+
+            audio_element.src = songs[index];
+            audio_element.play()
+            button.classList.replace("fa-play", "fa-pause")
+            play_pause.classList.replace("fa-play", "fa-pause")
+
+            currentPlayingIndex = index
+            album_name.innerText = songNames[index]
+
+        }
+        else {
+
+            if (audio_element.paused) {
+                audio_element.play()
+                button.classList.replace("fa-play", "fa-pause")
+                play_pause.classList.replace("fa-play", "fa-pause")
+            }
+            else {
+                audio_element.pause()
+                button.classList.replace("fa-pause", "fa-play")
+                play_pause.classList.replace("fa-pause", "fa-play")
+            }
+        }
+
+
+    })
+})
+
 
 play_pause.addEventListener("click", () => {
 
@@ -25,66 +81,26 @@ audio_element.addEventListener("timeupdate", () => {
     let progress_value = (audio_element.currentTime / audio_element.duration) * 100;
     console.log(progress_value)
     song_progress_bar.value = progress_value;
+    let display_time = audio_element.currentTime;
+    time.innerText = sec_to_min(display_time) +" / "+ sec_to_min(audio_element.duration) 
+
 })
 
+function sec_to_min(d){
+    var m = Math.floor(d/60)
+    var s = Math.floor(d%60)
 
-
-const song_details = {
-    0 : "./Songs/song2Devara.mp3",
-    1 : "./Songs/lightOfSevenSong2.mp3",
-    2 : "./Songs/song3KonteChuputho.mp3",
-    3 : "./Songs/song4TumHiHo.mp3",
-    6 : "./Songs/song7Evarevaro.mp3"
+    return m +":"+s
 }
 
-function add_play_icon(ele){
-    let container = document.getElementById("song_container");
-    let play_solid =  container.querySelectorAll("#play_solid"); 
-    play_solid[ele].style.display = "flex";
+audio_element.addEventListener("timeupdate", (ele) => {
+    let progress_value = (audio_element.currentTime / audio_element.duration) * 100;
+    song_progress_bar.value = progress_value;
+})
 
-    let audio_ele = new Audio(song_details[ele])
+song_progress_bar.addEventListener("change", (t) => {
 
-   let play_icon = container.querySelectorAll("#play_icon")
-   console.log(play_icon[ele])
-
-   
-   play_icon[ele].addEventListener("click",()=>{
-       
-       if(audio_ele.paused || audio_ele.currentTime == 0){
-           audio_ele.play();
-        }
-        else{
-            audio_ele.pause()
-        }
-        
-    })
-    
-    audio_ele.addEventListener("timeupdate",()=>{
-     let progress_value = (audio_ele.currentTime / audio_ele.duration)*100;
-     song_progress_bar.value = progress_value;
-    })
-   
-    song_progress_bar.addEventListener("change",(t)=>{
-
-        let n = song_progress_bar.value;
-        audio_ele.currentTime = (n*audio_ele.duration)/100;
-    })
-
-}
-
- function remove_play_icon(ele){
-    let container = document.getElementById("song_container");
-    let play_solid = container.querySelectorAll("#play_solid");
-    play_solid[ele].style.display = "none";
-
- }
-
-
-
-
-
-
-
-
-
-
+    let n = song_progress_bar.value;
+    // console.log(n)
+    audio_element.currentTime = (n * audio_element.duration) / 100;
+})
