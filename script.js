72940809a5bab9b3e651981c089ffbf63914f217
AJAY VATHAM <ajayvatham31@gmail.com>
2025-01-26 14:22:32 +0530

    added mute volume slider

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let play_pause = document.getElementById("play_pause");
 let audio_element = new Audio("./Songs/lightOfSevenSong2.mp3");
+
 let song_progress_bar = document.getElementById("song_progress_bar");
 let play_buttons = document.querySelectorAll("#play_solid i")
 let currentPlayingIndex = -1
@@ -9,6 +10,11 @@ let time = document.querySelector("#time")
 
 let forward = document.querySelector(".fa-forward")
 let backward = document.querySelector(".fa-backward")
+
+
+let volume_range = document.getElementById("volume_range1")
+
+let audio = new Audio();
 
 let songs = [
     "./Songs/song1.mp3", "./Songs/song2.mp3", "./Songs/song3.mp3", "./Songs/song4.mp3", "./Songs/song5.mp3", "./Songs/song6.mp3", "./Songs/song7.mp3",
@@ -20,6 +26,11 @@ let songNames = [
     "Chuttamlle", "Oh Priya Priya", "Konte Chooputho", "Tum Hi Ho", "Ban Ja Rani", "Ban Ja Rani Remix", "Animal(Telugu)", "Yeh Jawaani hai Deewani", "Peelings",
     "Panjaa", "Godari Gattu Meeda", "Radhika Radhika"
 ]
+
+
+
+
+
 
 //playButtons of albums
 play_buttons.forEach((button, index) => {
@@ -162,4 +173,63 @@ backward.addEventListener("click", () => {
         play_pause.classList.replace("fa-play","fa-pause")
         currentPlayingIndex = count
     }
+})
+
+
+let volume = document.getElementById("volume1")
+let volume_btn = document.querySelector(".fa-volume-low")
+
+
+volume_btn.addEventListener("click",(e)=>{
+
+   if(e.target.classList[1]==="fa-volume-low"){
+    audio_element.volume = 1.0;
+    volume_range.value = 1*100
+    volume_btn.classList.replace("fa-volume-low","fa-volume-high");
+   }
+   else if(e.target.classList[1] === "fa-volume-high"){
+    audio_element.volume = 0.0;
+    volume_range.value = 0*100
+    volume_btn.classList.replace("fa-volume-high","fa-volume-mute")
+   }
+   else{
+    audio_element.volume = 0.5
+    volume_range.value = 0.5*100
+    volume_btn.classList.replace("fa-volume-mute","fa-volume-low")
+   }
+   
+})
+
+
+
+volume_range.addEventListener("change",(t)=>{
+    let b = volume_range.value
+    audio_element.volume = b*0.01
+    
+    if(b==0){
+        volume_btn.classList.remove("fa-volume-high")
+        volume_btn.classList.remove("fa-volume-low")
+        volume_btn.classList.add("fa-volume-mute")
+        
+    }
+   else if(b==50){
+        volume_btn.classList.remove("fa-volume-high")
+        volume_btn.classList.remove("fa-volume-mute")
+        volume_btn.classList.add("fa-volume-low")
+        
+    }
+    
+  else if(b==100){
+        volume_btn.classList.remove("fa-volume-low")
+        volume_btn.classList.remove("fa-volume-mute")
+        volume_btn.classList.add("fa-volume-high")
+        
+    }
+    else{
+        volume_btn.classList.remove("fa-volume-high")
+        volume_btn.classList.remove("fa-volume-mute")
+        volume_btn.classList.add("fa-volume-low")
+
+    }
+
 })
